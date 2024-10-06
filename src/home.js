c4b61e679002d9c844829ee010628aaba615e0ca
Francois <francois.thullier98@gmail.com>
2024-10-06 17:48:17 +0200

    Added  wipePage function and eventListener to handle click

--- src/home.js
+++ src/home.js
@@ -3,7 +3,7 @@
     titleContainer=document.createElement("h1");
     paragraphContainer=document.createElement("p");
     
-    title="Restaurant";
+    title="La casa de la abuela";
     paragraphOne="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     
     paragraphContainer.textContent=paragraphOne;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
 
-console.log("Hello");
+
 import {} from './home.js';
+import {} from './menu.js';
 import "./styles.css";
+
+const tabButton=document.querySelectorAll("#nav-btn");
+
+const wipePage=  () => {
+
+    const mainContainer=document.querySelector("#content");
+   
+    tabButton.forEach((elem) =>
+        elem.addEventListener("click",() => {
+            mainContainer.replaceChildren();
+            console.log(mainContainer);
+            console.log("click");
+        })
+        
+    )
+    
+};
+
+console.log(wipePage());
